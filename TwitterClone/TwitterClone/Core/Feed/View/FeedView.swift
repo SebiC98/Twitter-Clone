04d9aed3959c2662c1dd65bd36594821d566144f
SebiC98 <sebastiancioata98@gmail.com>
2023-01-15 12:25:30 +0200

    Tweet Row View

---
+++ TwitterClone/TwitterClone/Core/Feed/View/FeedView.swift
@@ -0,0 +1,29 @@
+//
+//  FeedView.swift
+//  TwitterClone
+//
+//  Created by Sebastian Cioată on 15.01.2023.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        
+        ScrollView{
+            LazyVStack{
+                ForEach(0 ... 20, id: \.self){
+                    _ in
+                    TweetRowView()
+                }
+            }
+        }
+        
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
---
+++ TwitterClone/TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Sebastian Cioată on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
